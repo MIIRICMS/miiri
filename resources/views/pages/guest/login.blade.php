62a508e8cc3488f70931eddd51139d00a6550346
kunozga265 <kunozgamlowoka@gmail.com>
2023-10-02 12:49:00 +0200

    Adding user

--- resources/views/pages/guest/login.blade.php
+++ resources/views/pages/guest/login.blade.php
@@ -17,13 +17,24 @@
               <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
               <form class="forms-sample" method="post" action="{{route('login.post')}}">
                   @csrf
+
+                  <div class="mb-3">
+                      @if($errors->has('email'))
+                          <div class="error" style="color: red">{{ $errors->first('email') }}</div>
+                      @endif
+                          @if($errors->has('password'))
+                              <div class="error" style="color: red">{{ $errors->first('password') }}</div>
+                          @endif
+                  </div>
                 <div class="mb-3">
                   <label for="userEmail" class="form-label">Email address</label>
                   <input name="email" type="email" class="form-control" id="userEmail" placeholder="Email">
+
                 </div>
                 <div class="mb-3">
                   <label for="userPassword" class="form-label">Password</label>
                   <input name="password" type="password" class="form-control" id="userPassword" autocomplete="current-password" placeholder="Password">
+
                 </div>
                 <div class="form-check mb-3">
                   <input type="checkbox" class="form-check-input" id="authCheck">
